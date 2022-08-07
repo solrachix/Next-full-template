bce693f2647dbb9a175ba77760a24b505c336d8c
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2022-08-07 15:16:03 -0300

    feat: :sparkles: Adding cache in axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,17 @@
-import axios from 'axios'
+import { setup } from 'axios-cache-adapter'
 
 import Interceptors from '../configs/axiosInterceptors'
 
-const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`
+const api = setup({
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+
+  cache: {
+    maxAge: 15 * 60 * 1000, // 15 mins
+    exclude: {
+      // Only exclude POST, PUT, PATCH and DELETE methods from cache
+      methods: ['post', 'put', 'patch', 'delete']
+    }
+  }
 })
 
 Interceptors(api)
